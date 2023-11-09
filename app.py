3a1dd13cbb84f0cab5e33fcb5da45188e5ef72d7
Ika Komalasari <komalaika229@gmail.com>
2023-11-09 11:51:06 +0700

    First Commit

--- app.py
+++ app.py
@@ -11,32 +11,47 @@ import requests
 from datetime import datetime
 from bson import ObjectId
 
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
 app = Flask(__name__)
-
-password = 'sparta'
-cxn_str = f'mongodb+srv://test_ikakomalasari:{password}@test.nrkvr1l.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(cxn_str)
-
-db = client.dbsparta_plus_week2
 
 @app.route('/')
 def main():
-    words_result = db.words.find({}, {'_id': False})
+    words_result = db.words.find({}, {'_id':False})
     words = []
     for word in words_result:
         definition = word['definitions'][0]['shortdef']
         definition = definition if type(definition) is str else definition[0]
         words.append({
-            'word': word['word'],
+            'word' : word["word"],
             'definition': definition,
         })
-        msg = request.args.get('msg')
-    return render_template(
-        'index.html',
-        words=words,
-        msg = msg
-    )
+    msg = request.args.get('msg')
+    return render_template('index.html', words=words, msg=msg)
 
+
+@app.route('/error')
+def error():
+    word = request.args.get('word')
+    suggestions = request.args.get('suggestions')
+    if suggestions:
+        suggestions = suggestions.split(',')
+    return render_template(
+        'error.html',
+        word=word,
+        suggestions=suggestions
+    )
 
 @app.route('/detail/<keyword>')
 def detail(keyword):
@@ -47,29 +62,24 @@ def detail(keyword):
 
     if not definitions:
         return redirect(url_for(
-        'main',
-        msg=f'Could not find word "{keyword}"'
-    ))
-
-    if not definitions:
-        error_message = f'Could not find word "{keyword}"'
-        return render_template('eror.html', error_message=error_message)
+            'error',
+            word=keyword
+        ))
 
     if type(definitions[0]) is str:
-        suggestion = ','.join(definitions)
         return redirect(url_for(
-        'main',
-        msg=f'Could not find word "{keyword}", did you mean one of these words:{suggestion}'
+            'error',
+            word=keyword,
+            suggestions=','.join(definitions)
         ))
 
     status = request.args.get('status_give', 'new')
     return render_template(
         'detail.html', 
-        word=keyword,
+        word=keyword, 
         definitions=definitions,
         status=status
     )
-    
 
 @app.route('/api/save_word', methods=['POST'])
 def save_word():
@@ -80,9 +90,8 @@ def save_word():
     doc = {
         'word': word,
         'definitions': definitions,
-        'date': datetime.now().strftime('%Y%m%d'),
+        'date': datetime.now().strftime('%Y%m%d')
     }
-
     db.words.insert_one(doc)
 
     return jsonify({
@@ -91,7 +100,7 @@ def save_word():
     })
 
 @app.route('/api/delete_word', methods=['POST'])
-def detele_word():
+def delete_word():
     word = request.form.get('word_give')
     db.words.delete_one({'word': word})
     db.examples.delete_many({'word': word})
@@ -99,22 +108,21 @@ def detele_word():
         'result': 'success',
         'msg': f'the word, {word}, was deleted',
     })
+
 @app.route('/api/get_exs', methods=['GET'])
 def get_exs():
     word = request.args.get('word')
-    example_data = db.example.find({'word': word})
+    example_data = db.examples.find({'word': word})
     examples = []
     for example in example_data:
         examples.append({
             'example': example.get('example'),
             'id': str(example.get('_id')),
         })
-
     return jsonify({
         'result': 'success',
         'examples': examples
-        
-        })
+    })
 
 @app.route('/api/save_ex', methods=['POST'])
 def save_ex():
@@ -127,10 +135,8 @@ def save_ex():
     db.examples.insert_one(doc)
     return jsonify({
         'result': 'success',
-        'msg': f'Your example, {example}, for the word, {word} was saved',
-        
-        })
-
+        'msg': f'Your example, {example}, for the word, {word}, was saved!',
+    })
 
 @app.route('/api/delete_ex', methods=['POST'])
 def delete_ex():
@@ -139,9 +145,8 @@ def delete_ex():
     db.examples.delete_one({'_id': ObjectId(id)})
     return jsonify({
         'result': 'success',
-        'msg': f'Your word, {word}, was deleted!',
-        
-        })
+        'msg': f'Your example for the word, {word}, was deleted!',
+    })
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
